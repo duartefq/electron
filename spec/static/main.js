feb8028f90aaf879b22cd2a54d00713e9894267b
Kevin Sawicki <kevinsawicki@gmail.com>
2017-01-25 08:55:38 -0800

    Add failing spec for uncaught main process errors

--- spec/static/main.js
+++ spec/static/main.js
@@ -249,3 +249,18 @@ ipcMain.on('create-window-with-options-cycle', (event) => {
 ipcMain.on('prevent-next-new-window', (event, id) => {
   webContents.fromId(id).once('new-window', event => event.preventDefault())
 })
+
+ipcMain.on('handle-uncaught-exception', (event, message) => {
+  const listeners = process.listeners('uncaughtException')
+  process.removeAllListeners('uncaughtException')
+  process.on('uncaughtException', (error) => {
+    process.removeAllListeners('uncaughtException')
+    listeners.forEach((listener) => {
+      process.on('uncaughtException', listener)
+    })
+    event.returnValue = error.message
+  })
+  fs.readFile(__filename, () => {
+    throw new Error(message)
+  })
+})
